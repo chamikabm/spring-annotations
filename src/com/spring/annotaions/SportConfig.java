0460973fe4a78d0d18b3ce61ce81632c95d51631
chamikab <kasun.chamika@gmail.com>
2018-11-17 23:09:40 +0530

    Added changes for creating beans from Java Code.

--- src/com/spring/annotaions/SportConfig.java
+++ src/com/spring/annotaions/SportConfig.java
@@ -1,11 +1,23 @@
 package com.spring.annotaions;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.spring.annotaions") // This works exactly same as XML component scanning.
+//@ComponentScan("com.spring.annotaions") // We don't need to do component scan as we rely on following bean definitions.
 public class SportConfig {
 
+    // Define bean for sad fortune service.
+    @Bean  // <--- Here the actual bean id will be the method name.
+    public FortuneService sadFortuneService() {
+        return new SadFortuneService();
+    }
 
+
+    // Define bean for Swim Coach and Inject dependency.
+    @Bean // <--- Here the actual bean id will be the method name.
+    public Coach swimCoach() {
+        return new SwimCoach(sadFortuneService()); // We are calling above method to inject the bean.
+    }
 }
---
+++ src/com/spring/annotaions/SwimJavaConfigDemoApp.java
@@ -0,0 +1,18 @@
+package com.spring.annotaions;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SwimJavaConfigDemoApp {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(SportConfig.class);
+
+        Coach coach = context.getBean("swimCoach", Coach.class);
+
+        System.out.println(coach.getDailyFortune());
+        System.out.println(coach.getDailyWorkout());
+
+        context.close();
+    }
+}
