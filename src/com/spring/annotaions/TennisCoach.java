23d53c22692200e2d50b4dc771d6f1c8d4bab7af
chamikab <kasun.chamika@gmail.com>
2018-11-17 19:29:16 +0530

    Added method and feild auto wired changes.

--- src/com/spring/annotaions/TennisCoach.java
+++ src/com/spring/annotaions/TennisCoach.java
@@ -6,12 +6,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
-    @Autowired
+    // Default constructor not required but implemented for to check the initialization order.
+    public TennisCoach() {
+        System.out.println("Inside the default constructor.");
+    }
+
+/*    @Autowired
     public TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
-    }
+    }*/
+
+
+/*//     For this setter method you can have any name you want.
+    @Autowired
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("Inside the setFortune method.");
+        this.fortuneService = fortuneService;
+    }*/
 
     @Override
     public String getDailyWorkout() {
